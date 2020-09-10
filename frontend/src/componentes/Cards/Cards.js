a586ecb355650281a260cc369f8d857fa93d92e9
muriloviscondi <muriloviscondi@gmail.com>
2020-09-09 22:45:16 -0300

    removendo classe de cor materialize

--- frontend/src/componentes/Cards/Cards.js
+++ frontend/src/componentes/Cards/Cards.js
@@ -5,7 +5,7 @@ import style from './style.module.css';
 export default function Cards() {
   return (
     <div className="col s12 m4">
-      <div className="card cyan darken-4">
+      <div className="card">
         <div className="card-content center">
           <p className={`center ${style.titleCash}`}>R$ 1000,00</p>
           <p>Receitas</p>
