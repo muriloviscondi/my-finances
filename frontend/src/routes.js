dd21319022f566aad8ea865517dcb8227bfd8f43
muriloviscondi <muriloviscondi@gmail.com>
2020-09-09 22:42:32 -0300

    atualizando rotas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Financy from './pages/Finances/index';
+import Home from './pages/Finances/index';
+import Create from './pages/Create/index';
+import Update from './pages/Edit/index';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Financy} />
+        <Route path="/" exact component={Home} />
+        <Route path="/create" component={Create} />
+        <Route path="/update/:id" component={Update} />
       </Switch>
     </BrowserRouter>
   );
