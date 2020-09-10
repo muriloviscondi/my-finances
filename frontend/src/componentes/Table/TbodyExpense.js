26466691f195e2f618382accf2a05340797cbc6c
muriloviscondi <muriloviscondi@gmail.com>
2020-09-10 14:24:11 -0300

    conectando lista com banco de dados

--- frontend/src/componentes/Table/TbodyExpense.js
+++ frontend/src/componentes/Table/TbodyExpense.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { FiEdit2, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import style from './style.module.css';
 
 export default function TbodyExpense() {
+  const [financials, setFinancials] = useState([]);
+
+  useEffect(() => {
+    api.get('/').then((response) => {
+      setFinancials(response.data);
+    });
+  });
+
+  const expenses = [];
+
+  financials.forEach((financial) => {
+    if (financial.type.toLowerCase() === 'expense') {
+      expenses.push(financial);
+    }
+  });
+
+  async function handleDeleteExpense(id) {
+    try {
+      await api.delete(`/delete/${id}`);
+
+      setFinancials(financials.filter((financial) => financial.id !== id));
+    } catch (err) {
+      alert('Erro ao deletar despesa, tente novamente.');
+    }
+  }
+
   return (
-    <tr>
-      <td>1</td>
-      <td>05/06/2020</td>
-      <td>Salário</td>
-      <td>Recebimento de Salário</td>
-      <td>R$ 2.000,00</td>
-      <td>
-        <a href="/update/">
-          <FiEdit2 />
-        </a>
-      </td>
-      <td>
-        <a>
-          <FiTrash2 className={style.editIcon} />
-        </a>
-      </td>
-    </tr>
+    <>
+      {expenses.map((expense) => (
+        <tr key={expense.id}>
+          <td>{expense.id}</td>
+          <td>{expense.data}</td>
+          <td>{expense.name}</td>
+          <td>{expense.description}</td>
+          <td>
+            R${' '}
+            {Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(expense.value)}
+          </td>
+          <td>
+            <Link to={`/update/${expense.id}`}>
+              <FiEdit2 />
+            </Link>
+          </td>
+          <td>
+            <a onClick={() => handleDeleteExpense(expense.id)}>
+              <FiTrash2 className={style.editIcon} />
+            </a>
+          </td>
+        </tr>
+      ))}
+    </>
   );
 }
--- frontend/src/componentes/Table/TbodyRevenue.js
+++ frontend/src/componentes/Table/TbodyRevenue.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { FiEdit2, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import style from './style.module.css';
 
 export default function TbodyRevenue() {
+  const [financials, setFinancials] = useState([]);
+
+  useEffect(() => {
+    api.get('/').then((response) => {
+      setFinancials(response.data);
+    });
+  });
+
+  const revenues = [];
+
+  financials.forEach((financial) => {
+    if (financial.type.toLowerCase() === 'revenue') {
+      revenues.push(financial);
+    }
+  });
+
+  async function handleDeleteRevenue(id) {
+    try {
+      await api.delete(`/delete/${id}`);
+
+      setFinancials(financials.filter((financial) => financial.id !== id));
+    } catch (err) {
+      alert('Erro ao deletar receita, tente novamente.');
+    }
+  }
+
   return (
-    <tr>
-      <td>1</td>
-      <td>05/06/2020</td>
-      <td>Salário</td>
-      <td>Recebimento de Salário</td>
-      <td>R$ 2.000,00</td>
-      <td>
-        <a href="/update/">
-          <FiEdit2 />
-        </a>
-      </td>
-      <td>
-        <a>
-          <FiTrash2 className={style.editIcon} />
-        </a>
-      </td>
-    </tr>
+    <>
+      {revenues.map((revenue) => (
+        <tr key={revenue.id}>
+          <td>{revenue.id}</td>
+          <td>{revenue.data}</td>
+          <td>{revenue.name}</td>
+          <td>{revenue.description}</td>
+          <td>
+            R${' '}
+            {Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(revenue.value)}
+          </td>
+          <td>
+            <Link to={`/update/${revenue.id}`}>
+              <FiEdit2 />
+            </Link>
+          </td>
+          <td>
+            <a onClick={() => handleDeleteRevenue(revenue.id)}>
+              <FiTrash2 className={style.editIcon} />
+            </a>
+          </td>
+        </tr>
+      ))}
+    </>
   );
 }
